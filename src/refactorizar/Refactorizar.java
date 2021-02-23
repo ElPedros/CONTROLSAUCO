dcf45b546543ee7575031efc7f5c3b1be8046918
dam <dam@203P01>
2021-02-23 13:34:54 +0100

    Segundo paso

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -15,7 +15,7 @@ public class Refactorizar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("en la rama se crea la clase nueva");
     }
     
 }
