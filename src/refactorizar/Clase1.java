b071209e5e66e266115a76b8fe69427912cbe791
dam <dam@203P01>
2021-02-23 13:19:55 +0100

    Primer paso

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    int numero;
+    
+    public void hola(){
+        if(numero> 0){
+            System.out.println("Hola");
+        }
+    }
+}
